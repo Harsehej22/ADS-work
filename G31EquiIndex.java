550726ff14383eb081d4bf413c229daa8f7d94b2
Harsehej preet kaur <156522729+Harsehej22@users.noreply.github.com>
2026-02-07 22:50:59 +0530

    Update G31EquiIndex.java

--- G31EquiIndex.java
+++ G31EquiIndex.java
@@ -9,7 +9,7 @@ public class G31EquiIndex {
             prefix[i]=prefix[i-1]+arr[i];
         }
         suffix[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){  
             suffix[i]=suffix[i+1]+arr[i];
         }
         for(int i =0 ;i<n; i++){
